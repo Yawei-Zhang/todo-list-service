79722b741026ac36dcd78009681bbf80dde20024
yawei.zhang <yawei.zhang@rea-group.com>
2019-08-03 16:36:15 +0800

    Add Controller layer for items resource

--- src/main/java/com/example/todo/Controller/ItemController.java
+++ src/main/java/com/example/todo/Controller/ItemController.java
@@ -1,4 +1,23 @@
 package com.example.todo.Controller;
 
+import com.example.todo.Model.Item;
+import com.example.todo.Service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/items")
 public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
 }
--- src/test/java/com/example/todo/Controller/ItemControllerTest.java
+++ src/test/java/com/example/todo/Controller/ItemControllerTest.java
@@ -1,5 +1,45 @@
+package com.example.todo.Controller;
+
+import com.example.todo.Model.Item;
+import com.example.todo.Service.ItemService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Collection;
+import java.util.HashSet;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ItemControllerTest {
 
+    @Mock
+    private ItemService itemService;
+
+    @InjectMocks
+    private ItemController itemController;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void testGetAllItems() {
+        Collection<Item> expectedItems = new HashSet<Item>(){
+            {
+                add(new Item(1, "Read"));
+                add(new Item(2, "Write"));
+            }
+        };
+        when(itemService.getAllItems()).thenReturn(expectedItems);
+
+        Collection<Item> actualItems = itemController.getAllItems();
+
+        verify(itemService, times(1)).getAllItems();
+        assertEquals(expectedItems, actualItems);
+    }
 }
